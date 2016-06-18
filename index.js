01bfbfff2bd20ba771db9b6d421e802da5db8818
Nils Knappmeier <npm@knappi.org>
2016-06-18 22:27:24 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * thought-dev <https://github.com/nknapp/thought-dev>
+ *
+ * Copyright (c) 2016 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = thoughtDev
+/**
+ * Describe your module here
+ * @public
+ */
+function thoughtDev () {
+  // body
+}
---
+++ test/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * thought-dev <https://github.com/nknapp/thought-dev>
+ *
+ * Copyright (c) 2016 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var thoughtDev = require('../')
+
+describe('thought-dev:', function () {
+  // body
+})
